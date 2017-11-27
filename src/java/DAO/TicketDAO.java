fd59fc993723632c1e9d7d2e640ab6fa667d6539
tank3man <32251783+tank3man@users.noreply.github.com>
2017-11-27 15:59:21 -0600

    DAO´s

--- src/java/DAO/TicketDAO.java
+++ src/java/DAO/TicketDAO.java
@@ -5,10 +5,58 @@
  */
 package DAO;
 
+import bean.Persona;
+import bean.Producto;
+import bean.Reporte;
+import hbm.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author tank3
  */
 public class TicketDAO {
+    Session session;
+    
+    public TicketDAO() {
+        session = HibernateUtil.getLocalSession();
+    }
+    
+    public boolean saveProducto(Reporte prod){
+        try {
+            Transaction trans = this.session.beginTransaction();
+            this.session.save(prod);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean savePersona(Persona per){
+        try {
+            Transaction trans = this.session.beginTransaction();
+            this.session.save(per);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean saveReporte(Persona repo){
+        try {
+            Transaction trans = this.session.beginTransaction();
+            this.session.save(repo);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
     
 }
